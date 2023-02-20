060ff287cc34977c4a90266b48d80ab6b7474bc3
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2023-02-21 00:25:15 +0400

    Add components with handles function

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, TextField} from "@mui/material";
 
-function Header() {
+function Contact() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+
+        await fetch("http://localhost:3636/send-email", {
+            method: "POST",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+    };
+
     return (
-        <div style={{ marginTop: '20px'}}>
-            Contact
-        </div>
-    )
+        <form onSubmit={handleSubmit}>
+            <TextField
+                label="Name"
+                variant="outlined"
+                value={formData.name}
+                onChange={(e) => setFormData({...formData, name: e.target.value})}
+                required
+            />
+            <TextField
+                label="Email"
+                variant="outlined"
+                type="email"
+                value={formData.email}
+                onChange={(e) => setFormData({...formData, email: e.target.value})}
+                required
+            />
+            <TextField
+                label="Message"
+                variant="outlined"
+                multiline
+                rows={4}
+                value={formData.message}
+                onChange={(e) =>
+                    setFormData({...formData, message: e.target.value})
+                }
+                required
+            />
+            <Button variant="contained" color="primary" type="submit">
+                Send
+            </Button>
+        </form>
+    );
 }
 
-export default Header
+export default Contact;
