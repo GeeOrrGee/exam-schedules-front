b1d9072216cef531e08e8fa40963cd400a33d445
AkakiGurgenidze <agurg21@freeuni.edu.ge>
2023-02-21 01:35:33 +0400

    Change style and add alert when message sent

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,39 @@
 import React, {useState} from "react";
-import {Button, TextField} from "@mui/material";
+import {Button, TextField, Typography} from "@mui/material";
+
+interface Styles {
+    [key: string]: React.CSSProperties;
+}
+
+const styles: Styles = {
+
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        marginTop: "30px"
+    },
+    form: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "16px",
+        //border: "1px solid #3f51b5",
+        borderRadius: "4px",
+        maxWidth: "500px",
+    },
+    input: {
+        margin: "8px",
+        width: "100%",
+    },
+    button: {
+        marginTop: "16px",
+        alignSelf: "flex-end",
+    },
+    title: {
+        marginBottom: "16px",
+    },
+};
 
 function Contact() {
 
@@ -12,48 +46,74 @@ function Contact() {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
 
-        await fetch("http://localhost:3636/send-email", {
+        const response = await fetch("http://localhost:3636/send-email", {
             method: "POST",
             body: JSON.stringify(formData),
             headers: {
                 "Content-Type": "application/json",
             },
-        });
+        })
 
+        if (response.status == 200) {
+            alert("Your message was sent successfully!");
+            setFormData({
+                name: "",
+                email: "",
+                message: "",
+            });
+        } else {
+            alert(
+                "Sorry, there was an error sending your message. Please try again."
+            );
+        }
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <TextField
-                label="Name"
-                variant="outlined"
-                value={formData.name}
-                onChange={(e) => setFormData({...formData, name: e.target.value})}
-                required
-            />
-            <TextField
-                label="Email"
-                variant="outlined"
-                type="email"
-                value={formData.email}
-                onChange={(e) => setFormData({...formData, email: e.target.value})}
-                required
-            />
-            <TextField
-                label="Message"
-                variant="outlined"
-                multiline
-                rows={4}
-                value={formData.message}
-                onChange={(e) =>
-                    setFormData({...formData, message: e.target.value})
-                }
-                required
-            />
-            <Button variant="contained" color="primary" type="submit">
-                Send
-            </Button>
-        </form>
+        <div style={styles.root}>
+            <form style={styles.form} onSubmit={handleSubmit}>
+                <Typography variant="h5" style={styles.title}>
+                    Contact Us
+                </Typography>
+                <TextField
+                    label="Name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    style={styles.input}
+                    required
+                />
+                <TextField
+                    label="Email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    style={styles.input}
+                    required
+                />
+                <TextField
+                    label="Message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    multiline
+                    rows={4}
+                    style={styles.input}
+                    required
+                />
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                    style={styles.button}
+                >
+                    Send
+                </Button>
+            </form>
+        </div>
     );
 }
 
